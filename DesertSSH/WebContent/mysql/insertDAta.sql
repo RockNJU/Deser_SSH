0496a97e6e8abf25edd2c3bd98d7747591b587b1
2393147618@qq.com <wzce2393147618>
2016-06-24 00:29:57 +0800

    上传一些数据

---
+++ DesertSSH/WebContent/mysql/insertDAta.sql
@@ -0,0 +1,21 @@
+
+
+insert into product(name,category,img,img2,img3,price,count) values
+
+('方块布朗尼','糕点','images/方块布朗尼1.jpg','images/方块布朗尼2.jpg','images/方块布朗尼3.jpg','60.00',35),
+('花生奶油饼干','饼干','images/花生奶油饼干1.jpg','images/花生奶油饼干2.jpg','images/花生奶油饼干3.jpg','34.00',32),
+('黄油面包','面包','images/黄油面包1.jpg','images/黄油面包2.jpg','images/黄油面包3.jpg','24.00',60),
+('老婆饼','饼干','images/老婆饼1.jpg','images/老婆饼2.jpg','images/老婆饼3.jpg','24.00',98),
+('麻薯球','面包','images/麻薯球1.jpg','images/麻薯球2.jpg','images/麻薯球3.jpg','22.00',30),
+('马卡龙','饼干','images/马卡龙1.jpg','images/马卡龙2.jpg','images/马卡龙3.jpg','22.00',56),
+('蔓越莓奶酪面包','面包','images/蔓越莓奶酪面包1.jpg','images/蔓越莓奶酪面包2.jpg','images/蔓越莓奶酪面包3.jpg','38.00',30),
+('蔓越莓曲奇','面包','images/蔓越莓曲奇1.jpg','images/蔓越莓曲奇2.jpg','images/蔓越莓曲奇3.jpg','38.00',45),
+('抹茶饼干','饼干','images/抹茶饼干1.jpg','images/抹茶饼干2.jpg','images/抹茶饼干3.jpg','39.00',60),
+('抹茶曲奇','糕点','images/抹茶曲奇1.jpg','images/抹茶曲奇2.jpg','images/抹茶曲奇3.jpg','39.00',230),
+('奶酪面包','面包','images/奶酪面包1.jpg','images/奶酪面包2.jpg','images/奶酪面包3.jpg','39.00',23),
+('奶香椰丝球','糕点','images/奶香椰丝球1.jpg','images/奶香椰丝球2.jpg','images/奶香椰丝球3.jpg','39.00',43),
+('奶油威化','糕点','images/奶油威化1.jpg','images/奶油威化2.jpg','images/奶油威化3.jpg','39.00',43),
+('咸蛋酥黄','糕点','images/咸蛋酥黄1.jpg','images/咸蛋酥黄2.jpg','images/咸蛋酥黄3.jpg','39.00',43),
+
+('杏仁牛轧糖','糕点','images/杏仁牛轧糖1.jpg','images/杏仁牛轧糖2.jpg','images/杏仁牛轧糖33.jpg','39.00',43)
+
